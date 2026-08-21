dc4b882b77bcd4515d35c56821e6a0347d0c7d51
xiaolingsundberg <>
2026-08-21 16:20:54 +0000

    Challenge: Creating Odd and Even Logic

---
+++ conditional_statements_in_python/challenge_creating_odd_and_even_logic/main.py
@@ -0,0 +1,12 @@
+# Initial variables and values
+num = 8941 % 931
+result = 'blank'
+
+# Determine whether the number is odd or even
+if num == 1:
+    result = 'odd'
+else:
+    result = 'even'
+
+# Testing
+print("The number num is", result)
